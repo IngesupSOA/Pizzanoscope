40c0752060a4c8bfbf1a13ba0e3d064e60bde527
lmangeat <leon.mangeat@gmail.com>
2015-11-26 20:54:25 +0100

    order payal complete

---
+++ app.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var swig = require('swig');
+
+var config = require('./config');
+
+var routes = require('./routes/index');
+var order = require('./routes/order');
+
+var app = express();
+
+///////////////////////////////////////
+///// CONFIGURATION  //////////////////
+///////////////////////////////////////
+var port = process.env.PORT || 3000; // used to create, sign, and verify tokens
+mongoose.connect(config.database.server, config.database.db, function(err){
+  if(err) console.log(err);
+
+  console.log("Connected to the database");
+});
+
+// view engine setup
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/order', order);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    console.log(err.message);
+    console.log(err.stack);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
